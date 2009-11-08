e766a5a84638660876cf0b0a7479ee9bb15c7233
El Skwid (Don Morrison) <elskwid@gmail.com>
2009-11-08 00:15:24 -0800

    Use ri-cal gem

--- lib/brical.rb
+++ lib/brical.rb
@@ -2,7 +2,8 @@ __DIR__ = File.dirname(__FILE__)
 $:.unshift "#{__DIR__}/brical", *Dir["#{__DIR__}/../vendor/**/lib"].to_a
 
 require "rubygems"
-require "icalendar"
+require "tzinfo"
+require "ri_cal"
 require "sinatra/base"
 require "open-uri"
 
@@ -21,30 +22,29 @@ module Brical
     end
 
     get "/:org.ical" do
-      calendar = Icalendar::Calendar.new
-      calendar.product_id = "cityofremo.com/ical"
-      calendar.custom_property("X-WR-CALNAME;VALUE=TEXT", "City of Remo 2009")
-      calendar.custom_property("X-WR-TIMEZONE;VALUE=TEXT", "US/Pacific")
-            
-      feed.entries.each do |entry|
-        ev = EbEvent.new(entry)
-
-        calendar.event do
-          summary     ev.title
-          description ev.summary
-          location    ev.where
-          url         ev.links.join(",")
-          uid         ev.id
-          dtstart     ev.start
-          dtend       ev.end
-          dtstamp     ev.published
-          klass       "PUBLIC"
+      RiCal::PropertyValue::DateTime.default_tzid = 'US/Pacific'      
+      cal = RiCal.Calendar do |calendar|
+        # calendar.product_id = "cityofremo.com/ical"
+        # calendar.custom_property("X-WR-CALNAME;VALUE=TEXT", "City of Remo 2009")
+        # calendar.custom_property("X-WR-TIMEZONE;VALUE=TEXT", "US/Pacific")
+        feed.entries.each do |entry|
+          ev = EbEvent.new(entry)
+          calendar.event do
+            summary         ev.title
+            description     ev.summary
+            location        ev.where
+            url             ev.links.join(",")
+            uid             ev.id
+            dtstart         ev.start
+            dtend           ev.end
+            dtstamp         ev.published if ev.published
+            security_class "PUBLIC"
+          end
         end
-        
       end
-
+      
       content_type "text/calendar"
-      body         calendar.to_ical
+      body  cal.export
     end
 
     # just dump out html so we can see something
